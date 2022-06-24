88964f73da0efcec9d4e1569750676d9e1eca814
Ali Atallaoui <43540967+aliatallaoui@users.noreply.github.com>
2022-06-24 02:23:48 +0100

    inital project commitment

---
+++ app/Http/Controllers/CircleCIController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WebhookNotification;
+use Illuminate\Http\{JsonResponse, Response};
+
+class CircleCIController extends Controller {
+
+    public function getAllNotifications()
+    : JsonResponse {
+
+        return response()->json();
+    }
+
+    public function handleNotification()
+    : JsonResponse {
+
+        return response()
+            ->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+}
