922466b2eab4aaf23f1c43df2d60100cc69f2b6c
Ali Atallaoui <43540967+aliatallaoui@users.noreply.github.com>
2022-06-24 02:51:02 +0100

    Implement Webhook for CircleCI

--- app/Http/Controllers/CircleCIController.php
+++ app/Http/Controllers/CircleCIController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\CircleCINotificationHelper;
 use App\Models\WebhookNotification;
-use Illuminate\Http\{JsonResponse, Response};
+use Illuminate\Http\{JsonResponse, Request, Response};
 
 class CircleCIController extends Controller {
 
     public function getAllNotifications()
     : JsonResponse {
 
-        return response()->json();
+        return response()->json(WebhookNotification::all());
     }
 
-    public function handleNotification()
+    public function handleNotification(Request $request)
     : JsonResponse {
+
+        CircleCINotificationHelper::handle($request);
 
         return response()
             ->json(null, Response::HTTP_NO_CONTENT);
